9be8390138335bb00e9553f2dc9d2e3079ab3ad6
Kauêh vitali Bento <67390551+KauehVBK@users.noreply.github.com>
2022-06-20 12:26:17 -0300

    Update ex2.java

--- ex2.java
+++ ex2.java
@@ -10,25 +10,25 @@ public class ex2 {
         try (Scanner entrada = new Scanner(System.in)) {
             Double h, bMaior, bMenor, area; 
 
-            System.out.println("========= Dados ========="); 
+            System.out.println("************ Dados ****************"); 
 
-            System.out.print("Informe a altura: ");
+            System.out.print("Digite a altura: ");
             h = entrada.nextDouble();
 
-            System.out.print("Informe a base menor: ");
+            System.out.print("Digite a base menor: ");
             bMenor = entrada.nextDouble();
 
-            System.out.print("Informe a base maior: ");
+            System.out.print("Digite a base maior: ");
             bMaior = entrada.nextDouble();
             
             area = (h * (bMenor + bMaior)) / 2;
 
-            System.out.println("========= Resultados ========="); 
-            System.out.println("Altura - > " + h); 
-            System.out.println("Base menor - > " + bMenor); 
-            System.out.println("Base menor - > " + bMaior); 
-            System.out.println("Área - > " + area); 
-            System.out.println("Área exata - > " + Math.round(area));
+            System.out.println("*************** Resultados ******************"); 
+            System.out.println("Altura: " + h); 
+            System.out.println("Base menor: " + bMenor); 
+            System.out.println("Base menor: " + bMaior); 
+            System.out.println("Área: " + area); 
+            System.out.println("Área exata: " + Math.round(area));
         }
     }
 
